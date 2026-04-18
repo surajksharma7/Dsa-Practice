93dc93f68c861b74b42f4f60087ba997d8a085c3
Suraj Kumar <surajksharma2304@gmail.com>
2026-04-18 11:15:35 +0530

    solved questions

---
+++ Data Structure/Intro to Cpp/02_dataTypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int count = 5;
+    float share = 3.14;
+    char ch = 'A'; // this will always be in single quote 
+    double weights = 55.69887;
+    bool ans = true;
+    bool isChild = 1;
+    
+
+}
